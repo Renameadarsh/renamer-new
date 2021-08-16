4fe92fe06b535d7413a6547269ff962b484d872b
Renameadarsh <88626710+Renameadarsh@users.noreply.github.com>
2021-08-16 19:10:46 +0530

    Update commands.py

--- renamer/plugins/commands.py
+++ renamer/plugins/commands.py
@@ -132,7 +132,7 @@ async def reset_user(c, m):
 
 ################## login command ##################
 
-@RenamerAdarshs.on_message(filters.command('login') & filters.incoming & filters.private)
+@RenamerAdarsh.on_message(filters.command('login') & filters.incoming & filters.private)
 async def password(c, m):
     if Config.BOT_PASSWORD:
         if m.from_user.id in Config.AUTH_USERS:
