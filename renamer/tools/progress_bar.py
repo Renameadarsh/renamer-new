e484ef1ed5264aaf282d79e5f07faf8dcefe190c
Renameadarsh <88626710+Renameadarsh@users.noreply.github.com>
2021-08-23 14:30:32 +0530

    Update progress_bar.py

--- renamer/tools/progress_bar.py
+++ renamer/tools/progress_bar.py
@@ -1,6 +1,6 @@
 import logging
 logger = logging.getLogger(__name__)
-
+from ..config import Config
 import os
 import math
 import time
@@ -19,8 +19,8 @@ async def progress_bar(current, total, status_msg, start, msg):
         time_to_complete = round(((total - current) / speed)) * 1000
         time_to_complete = TimeFormatter(time_to_complete)
         progressbar = "[{0}{1}]".format(\
-            ''.join([f"{ ruk }" for i in range(math.floor(percentage / 10))]),
-            ''.join([f"{ ruk }" for i in range(10 - math.floor(percentage / 10))])
+            ''.join([f"{ FINISHED }" for i in range(math.floor(percentage / 10))]),
+            ''.join([f"{ UNFINISHED }" for i in range(10 - math.floor(percentage / 10))])
             )
         current_message = f"""**{status_msg}** {round(percentage, 2)}%
 {progressbar}
